b0edc1ee97c61de5b351507a49981381f111a957
Camilo <camilohi.1196@gmail.com>
2024-04-03 20:16:23 -0500

    Ejercicio 2 y 3

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,7 @@
+console.log("Ejercicio 1");
+const num = 10;
+if (num % 2 ==  0){
+    console.log(`el numero ${num} es par`);
+}else{
+    console.log(`el numero ${num} no es par`);
+}
---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,9 @@
+console.log("---- Ejercicio 2 ----");
+const num = -10;
+if(num > 0){
+    console.log(`El numero ${num} es positivo`);
+}if(num < 0){
+    console.log(`el numero ${num} es impar`)
+}else{
+    console.log(`el numero ${num} es cero`)
+}
