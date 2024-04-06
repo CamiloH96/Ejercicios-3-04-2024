658823c9594c69d815d6e0739651d369a2f8ae9e
Dark Doctor <camilohi.1196@gmail.com>
2024-04-05 21:53:19 -0500

    creo que ejercicio 6 y 10 no recuerdo

--- Ejercicio10/ejercicio10.js
+++ Ejercicio10/ejercicio10.js
@@ -1,1 +1,15 @@
 console.log("---- Ejercicio 10 ----");
+
+const renta=parseInt(process.argv[2]);
+
+if(renta<=10000){
+    console.log(`Su impositivo es del 5% de su renta de ${renta}`);
+}if(renta>=10001 & renta<=20000){
+    console.log(`Su impositivo es del 15% de su renta de ${renta}`)
+}if(renta>=20001 & renta<=35000){
+    console.log(`Su impositivo es del 20% de su renta de ${renta}`)
+}if(renta>=35001 & renta<=60000){
+    console.log(`Su impositivo es del 30% de su renta de ${renta}`)
+}if(renta>=60001){
+    console.log(`Su impositivo es del 45% de su renta de ${renta}`)
+}
--- Ejercicio6/ejercicio6.js
+++ Ejercicio6/ejercicio6.js
@@ -1,1 +1,16 @@
 console.log("---- Ejercicio 6 ----");
+
+const numero_Articulos=parseInt(process.argv[2]);
+const precio_Unidad=parseInt(process.argv[3]);
+const precio_Total = numero_Articulos * precio_Unidad;
+
+if (numero_Articulos > 10 && precio_Unidad > 40) {
+    const descuento=precio_Total * 0.15;
+    const total=precio_Total-descuento;
+    console.log(`Numero de articulos ${numero_Articulos}, Precio por unidad ${precio_Unidad}`);
+    console.log(`Precio sin el descuento, ${precio_Total} - descuento ${descuento}`);
+    console.log(`Se aplica el descuento del 15%, la cantidad a pagar es: ${total} euros.`);
+}else{
+    console.log(`Numero de articulos ${numero_Articulos}, Precio por unidad ${precio_Unidad}`);
+    console.log(`No se aplica el descuento, la cantidad a pagar es: ${precio_Total} euros.`);
+}
