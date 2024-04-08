6aa68e226e791b3f7ac2c5908fc97349f0c30f5f
Dark Doctor <camilohi.1196@gmail.com>
2024-04-07 21:34:50 -0500

    Ejercicio9

--- Ejercicio9/ejercicio9.js
+++ Ejercicio9/ejercicio9.js
@@ -1,1 +1,15 @@
 console.log("---- Ejercicio 9 ----");
+
+const nombre = process.argv[2];
+const sexo = process.argv[3];
+
+let grupo;
+
+if ((sexo === 'h' && nombre.toUpperCase().charCodeAt(0) > 'N'.charCodeAt(0)) ||
+    (sexo === 'm' && nombre.toUpperCase().charCodeAt(0) < 'M'.charCodeAt(0))) {
+  grupo = 'A';
+} else {
+  grupo = 'B';
+}
+
+console.log(`Perteneces al grupo ${grupo}`);
